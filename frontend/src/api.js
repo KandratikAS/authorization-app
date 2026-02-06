aacc895d75bc058c6148f7c93e3ba4ab0e280fd6
Archi <Archi@MacBook-Air.local>
2026-02-06 16:34:17 +0300

    add backend.render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const raw = import.meta.env.VITE_API_URL;
+const BASE_URL = raw
+  ? (/\/api\/?$/.test(raw) ? raw.replace(/\/+$/, '') : raw.replace(/\/+$/, '') + '/api')
+  : 'http://127.0.0.1:3001/api';
+
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:3001/api'
+  baseURL: BASE_URL
 });
 
 api.interceptors.request.use(config => {
